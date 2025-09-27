62aa7a196b154d797c35b321644f4adfb8e60808
hiteshkhatic <ahiteshkhatic@gmail.com>
2025-09-28 01:27:23 +0530

    defined route for fetchina the record of specific user

--- src/routes/supplierRoutes.js
+++ src/routes/supplierRoutes.js
@@ -12,4 +12,21 @@ router.get("/readall", async (req, res) => {
   }
 })
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params
+  try {
+    const { rows } = await db.query(
+      "SELECT * FROM supplier WHERE supplier_id = $1;",
+      [id]
+    )
+    if (rows.length === 0) {
+        return res.status(404).json("No data found");
+    }
+    res.status(200).json(rows[0])
+
+  } catch (error) {
+    console.error("Error while fetching recored!", error)
+    return res.status(500).json({ error: "Server Error" })
+  }
+})
 module.exports = router

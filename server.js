3f7ff3fa6ef2e39eb6ffe7d60933adb3e65b9007
hiteshkhatic <ahiteshkhatic@gmail.com>
2025-09-27 23:13:55 +0530

    imported itemRotes and use it

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const categoryRoutes = require("./src/routes/categoryRoutes.js")
+const itemRoutes = require('./src/routes/itemRoutes.js')
 
 app.use(express.json())
 app.use("/category", categoryRoutes)
+app.use("/item", itemRoutes)
 
 const PORT = process.env.PORT
 app.listen(PORT || 5501, () => console.log(`Server is running on PORT ${PORT}`))

a950e2aaca0c596eb5c62c2077bae124f0c760f0
hiteshkhatic <ahiteshkhatic@gmail.com>
2025-09-28 02:08:03 +0530

    impoted route of suppliers and use it

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const express = require("express")
 const app = express()
 const categoryRoutes = require("./src/routes/categoryRoutes.js")
 const itemRoutes = require('./src/routes/itemRoutes.js')
+const supplierRoutes = require("./src/routes/supplierRoutes.js")
 
 app.use(express.json())
 app.use("/category", categoryRoutes)
 app.use("/item", itemRoutes)
+app.use("/supplier", supplierRoutes)
 
 const PORT = process.env.PORT
 app.listen(PORT || 5501, () => console.log(`Server is running on PORT ${PORT}`))

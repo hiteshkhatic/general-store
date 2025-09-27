ffe4d36bd8429b555e5e5d07fd8882b00a1137e1
hiteshkhatic <ahiteshkhatic@gmail.com>
2025-09-27 21:19:48 +0530

    update a piece of data using PUT Method, router defined updating records

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -29,4 +29,25 @@ router.post("/create", async (req, res) => {
   }
 })
 
+router.put("/:id", async (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+  if (!name) {
+    return res.status(400).json({ error: "Category name required to update" })
+  }
+  try {
+    const { rows } = await db.query(
+      "UPDATE category SET category_name = $1 WHERE category_id = $2 RETURNING *; ",
+      [name, id]
+    )
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "Category not found" })
+    }
+    res.status(200).json(rows[0])
+  } catch (error) {
+    console.error("Error updating category: ", error)
+    res.status(500).json({ error: "Internal Server Error" })
+  }
+})
+
 module.exports = router

735211bfc4b1797b3002e1c73a99fa9f701ac26f
hiteshkhatic <ahiteshkhatic@gmail.com>
2025-09-28 19:40:27 +0530

    added purchaseRoutes and using it for routing

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const app = express()
 const categoryRoutes = require("./src/routes/categoryRoutes.js")
 const itemRoutes = require('./src/routes/itemRoutes.js')
 const supplierRoutes = require("./src/routes/supplierRoutes.js")
+const purchaseRoutes = require("./src/routes/purchaseRoutes.js")
 
 app.use(express.json())
 app.use("/category", categoryRoutes)
 app.use("/item", itemRoutes)
 app.use("/supplier", supplierRoutes)
+app.use("/purchase", purchaseRoutes)
 
 const PORT = process.env.PORT
 app.listen(PORT || 5501, () => console.log(`Server is running on PORT ${PORT}`))
